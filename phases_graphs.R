fd8c0c8e58acb29a57b10f5731f43e12f9350d7f
Virew0rm <jcornils@msn.com>
2018-10-08 18:03:24 +0200

    add another plot 4075 with only accelerometry
    and one for 4084 with added asl

--- phases_graphs.R
+++ phases_graphs.R
@@ -4,13 +4,17 @@ library("ggplot2")
 
 #getting an idea of what the plot looks like
 plot(z~datetime, data=ACC_GPS_4075_clean)
+par(new=T)
+plot(asl~datetime, data=ACC_GPS_4075_clean, type="l", col="blue")
 
 head(ACC_GPS_4075_gr)
 plot(z~datetime, data=ACC_GPS_4075_gr, type="l")
 
+
+
 #plotting start phase ggplot####
 setEPS()
-postscript("first_start4075_2014_08_28.eps")
+postscript("first_start_4075_2014_08_28.eps")
 
 ACC_GPS_4075_gr<- subset(ACC_GPS_4075_clean, datetime>=as.POSIXct("2014-08-28 07:39:30"))
 ACC_GPS_4075_gr<- subset(ACC_GPS_4075_gr, datetime<=as.POSIXct("2014-08-28 07:40:30"))
@@ -20,25 +24,51 @@ ACC_GPS_4075_gr$time.y<- as.character(ACC_GPS_4075_gr$time.y)
 is.character(ACC_GPS_4075_gr$time.y)
 
 ggplot(data=ACC_GPS_4075_gr, aes(x=time.y, y=z))+
-  scale_x_discrete(name ="time",breaks=c("07:39:30","07:39:45","07:40:00","07:40:15","07:40:30"),labels=c("07:39:30","07:39:45","07:40:00","07:40:15","07:40:30"))+
-  scale_y_continuous(name="z-axis")+
- #warum geht die geschwindigkeit nicht rauf??????
-   geom_point(data=ACC_GPS_4075_gr, aes(x=time.y,y=speed), color="blue")+
-  geom_line(data=ACC_GPS_4075_gr,aes(x=time.y,y=z), color="red")+
-    theme(plot.margin = margin(10, 30, 20, 20), panel.background =element_rect(fill="white"), axis.line = element_line(color="black", size = 0.5))+
+  geom_line(data=ACC_GPS_4075_gr,aes(), color="red")+
+   scale_x_discrete(name ="time",breaks=c("07:39:30","07:39:45","07:40:00","07:40:15","07:40:30"),labels=c("07:39:30","07:39:45","07:40:00","07:40:15","07:40:30"))+
+  scale_y_continuous(name="z-axis")+ 
+  theme(plot.margin = margin(10, 30, 20, 20), panel.background =element_rect(fill="white"), axis.line = element_line(color="black", size = 0.5))+
   annotate("segment", x = 0, xend = 27.5, y = 1, yend = 1, colour = "red")  +
   annotate("text", x =15, y =-27,  colour = "red",label = "sitting", size=3)  +
   annotate("segment", x = 28.5, xend = 38, y = 1, yend = 1, colour = "blue")  +
-annotate("text", x = 33, y =-27,  colour = "blue",label = "startphase", size=3)  +
-annotate("segment", x = 38.5, xend = 60, y = 1, yend = 1, colour = "darkgreen")  +
+  annotate("text", x = 33, y =-27,  colour = "blue",label = "startphase", size=3)  +
+  annotate("segment", x = 38.5, xend = 60, y = 1, yend = 1, colour = "darkgreen")  +
   annotate("text", x = 49, y =-27,  colour = "darkgreen",label = "flying", size=3) 
 
 dev.off()
 
 
-#plotting landing phase ggplot####
+
+#plotting start phase with speed ggplot####
 setEPS()
-postscript("landing_4075_2014_08_28.eps")
+postscript("first_start_with_speed_4075_2014_08_28.eps")
+
+ACC_GPS_4075_gr<- subset(ACC_GPS_4075_clean, datetime>=as.POSIXct("2014-08-28 07:39:30"))
+ACC_GPS_4075_gr<- subset(ACC_GPS_4075_gr, datetime<=as.POSIXct("2014-08-28 07:40:30"))
+
+ACC_GPS_4075_gr$time.y<- as.character(ACC_GPS_4075_gr$time.y)
+
+is.character(ACC_GPS_4075_gr$time.y)
+
+ggplot(data=ACC_GPS_4075_gr, aes(x=time.y, y=z))+
+  geom_line(data=ACC_GPS_4075_gr,aes(), color="red")+
+  geom_point(aes(x=ACC_GPS_4075_gr$time.y,y=ACC_GPS_4075_gr$speed*100), color="blue")+
+  scale_x_discrete(name ="time",breaks=c("07:39:30","07:39:45","07:40:00","07:40:15","07:40:30"),labels=c("07:39:30","07:39:45","07:40:00","07:40:15","07:40:30"))+
+  scale_y_continuous(name="z-axis", sec.axis = sec_axis(~./100, name = "asl"))+ 
+  theme(plot.margin = margin(10, 30, 20, 20), panel.background =element_rect(fill="white"), axis.line = element_line(color="black", size = 0.5))+
+  annotate("segment", x = 0, xend = 27.5, y = 1, yend = 1, colour = "red")  +
+  annotate("text", x =15, y =-27,  colour = "red",label = "sitting", size=3)  +
+  annotate("segment", x = 28.5, xend = 38, y = 1, yend = 1, colour = "blue")  +
+annotate("text", x = 33, y =-27,  colour = "blue",label = "startphase", size=3)  +
+annotate("segment", x = 38.5, xend = 60, y = 1, yend = 1, colour = "darkgreen")  +
+annotate("text", x = 49, y =-27,  colour = "darkgreen",label = "flying", size=3) 
+
+dev.off()
+
+
+#plotting landing phase ggplot with speed####
+setEPS()
+postscript("landing_with_speed_4075_2014_08_28.eps")
 
 
 ACC_GPS_4075_gr<- subset(ACC_GPS_4075_clean, datetime>=as.POSIXct("2014-08-28 10:27:00"))
@@ -50,10 +80,11 @@ is.character(ACC_GPS_4075_gr$time.y)
 
 
 ggplot(data=ACC_GPS_4075_gr, aes(x=time.y, y=z))+
-  scale_x_discrete(name ="time",breaks=c("10:27:00","10:28:00","10:29:00","10:30:00","10:31:00","10:32:00","10:33:00","10:34:00"),labels=c("10:27:00","10:28:00","10:29:00","10:30:00","10:31:00","10:32:00","10:33:00","10:34:00"))+
-  scale_y_continuous(name="z-axis")+
   geom_line(aes())+
-  theme(plot.margin = margin(10, 30, 20, 20), panel.background =element_rect(fill="white"), axis.line = element_line(color="black", size = 0.5))+
+geom_point(aes(x=ACC_GPS_4075_gr$time.y,y=ACC_GPS_4075_gr$speed*100), color="blue")+
+  scale_x_discrete(name ="time",breaks=c("10:27:00","10:28:00","10:29:00","10:30:00","10:31:00","10:32:00","10:33:00","10:34:00"),labels=c("10:27:00","10:28:00","10:29:00","10:30:00","10:31:00","10:32:00","10:33:00","10:34:00"))+
+  scale_y_continuous(name="z-axis", sec.axis = sec_axis(~./100, name = "speed", breaks=c(0,10,20), labels=c("0", "10", "20")))+
+    theme(plot.margin = margin(10, 30, 20, 20), panel.background =element_rect(fill="white"), axis.line = element_line(color="black", size = 0.5))+
   annotate("segment", x = 0, xend = 120, y = 550, yend =550, colour = "darkgreen")  +
   annotate("text", x =70, y =500,  colour = "darkgreen",label = "flying", size=3)  +
   
@@ -68,7 +99,38 @@ ggplot(data=ACC_GPS_4075_gr, aes(x=time.y, y=z))+
 
 dev.off()
 
+#plotting landing phase ggplot with asl####
+setEPS()
+postscript("landing_with_asl_4075_2014_08_28.eps")
 
+
+ACC_GPS_4075_gr<- subset(ACC_GPS_4075_clean, datetime>=as.POSIXct("2014-08-28 10:27:00"))
+ACC_GPS_4075_gr<- subset(ACC_GPS_4075_gr, datetime<=as.POSIXct("2014-08-28 10:34:00"))
+
+ACC_GPS_4075_gr$time.y<- as.character(ACC_GPS_4075_gr$time.y)
+
+is.character(ACC_GPS_4075_gr$time.y)
+
+
+ggplot(data=ACC_GPS_4075_gr, aes(x=time.y, y=z))+
+  geom_line(aes())+
+  geom_point(aes(x=ACC_GPS_4075_gr$time.y,y=ACC_GPS_4075_gr$asl*10), color="blue")+
+  scale_x_discrete(name ="time",breaks=c("10:27:00","10:28:00","10:29:00","10:30:00","10:31:00","10:32:00","10:33:00","10:34:00"),labels=c("10:27:00","10:28:00","10:29:00","10:30:00","10:31:00","10:32:00","10:33:00","10:34:00"))+
+  scale_y_continuous(name="z-axis", sec.axis = sec_axis(~./10, name = "asl", breaks=c(0,50,100,150,200,250,300), labels=c("0", "50", "100","150", "200", "250", "300")))+
+  theme(plot.margin = margin(10, 30, 20, 20), panel.background =element_rect(fill="white"), axis.line = element_line(color="black", size = 0.5))+
+  annotate("segment", x = 0, xend = 120, y = 100, yend =100, colour = "darkgreen")  +
+  annotate("text", x =70, y =0,  colour = "darkgreen",label = "flying", size=3)  +
+  
+  annotate("segment", x = 122, xend = 300, y = 100, yend = 100, colour = "blue")  +
+  annotate("text", x = 200, y =0,  colour = "blue",label = "landing", size=3)  +
+  
+  annotate("segment", x = 302, xend = 350, y = 100, yend = 100, colour = "purple")  +
+  annotate("text", x = 330, y =0,  colour = "purple",label = "short flight/jump", size=3) +
+  
+  annotate("segment", x =352, xend = 400, y = 100, yend = 100, colour = "red") +
+  annotate("text", x = 380, y =0,  colour = "red",label = "sitting", size=3) 
+
+dev.off()
 #4078##### not finished yet######
 
 plot(z~datetime, data=ACC_GPS_4078_clean)
@@ -159,8 +221,8 @@ setEPS()
 postscript("landing_4084_2014_08_30.eps")
 
 
-ACC_GPS_4084_gr<- subset(ACC_GPS_4084_clean, datetime>=as.POSIXct("2014-08-30 07:30:00"))
-ACC_GPS_4084_gr<- subset(ACC_GPS_4084_gr, datetime<=as.POSIXct("2014-08-30 08:55:00"))
+ACC_GPS_4084_gr<- subset(ACC_GPS_4084_clean, datetime>=as.POSIXct("2014-08-30 10:28:00"))
+ACC_GPS_4084_gr<- subset(ACC_GPS_4084_gr, datetime<=as.POSIXct("2014-08-30 10:34:00"))
 plot(z~datetime, data=ACC_GPS_4084_gr, type="l")
 
 ACC_GPS_4084_gr$time.y<- as.character(ACC_GPS_4075_gr$time.y)
